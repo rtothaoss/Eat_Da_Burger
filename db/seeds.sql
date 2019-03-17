13696b36e8827f45261271070e8f66047d1b5539
Ross carmack <rosscarmack@Rosss-MBP.fios-router.home>
2019-03-17 17:12:23 -0500

    first push

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Cheeseburger');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Double Cheeseburger');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Triple Cheeseburger');
