e74c4ac08f8283482145567162282c2ea6935b79
Ross carmack <rosscarmack@Rosss-MBP.fios-router.home>
2019-03-17 22:14:53 -0500

    finished burgers controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,11 +5,44 @@ var router = express.Router();
 var burger = require('../model/burger.js')
 
 
-
-
-
-
-
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      var object = {
+        burger: data
+      };
+      console.log(object);
+      res.render("index", object);
+    });
+  });
+  
+  router.post("/api/burgers", function(req, res) {
+    burger.insert([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    cat.update({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
 
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -2,17 +2,17 @@ var orm = require('../config/orm.js')
 
 var burger = {
 
-    selectAll: function (cb) {
+    all: function (cb) {
         orm.selectAll("burgers", function (res) {
             cb(res);
         });
     },
-    insertOne: function (cols, vals, cb) {
+    insert: function (cols, vals, cb) {
         orm.insertOne("burgers", cols, vals, function (res) {
             cb(res);
         });
     },
-    updateOne: function (objColVals, condition, cb) {
+    update: function (objColVals, condition, cb) {
         orm.updateOne("burgers", objColVals, condition, function (res) {
             cb(res);
         });
